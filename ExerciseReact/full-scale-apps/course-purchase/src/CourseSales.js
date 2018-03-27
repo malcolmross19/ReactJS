cbdc0c76e809fc6a0e73b61fc6f50cdc7e920e68
Malcolm Ross <MJRoss@student.fullsail.edu>
2018-03-27 11:18:28 -0400

    Modified CourseSales.js
    Mapped the list of courses from the props and added the formatting for individual courses in the return statement.

--- ExerciseReact/full-scale-apps/course-purchase/src/CourseSales.js
+++ ExerciseReact/full-scale-apps/course-purchase/src/CourseSales.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Course from './Course';
 
 class CourseSales extends Component {
   sumPrice(price){
@@ -17,9 +18,17 @@ class CourseSales extends Component {
 
   render(){
     console.log(this.props.items)
+    var courses = this.props.items.map((item, i) => {
+      return <Course name={item.name} price={item.price}
+          key={i} sumPrice={this.sumPrice} active={item.active} />
+    });
     return(
       <div>
-
+          <h1>You can buy courses: </h1>
+          <div id="courses">
+              {courses}
+              <p id="total"> Total: <b>{this.state.total}</b></p>
+          </div>
       </div>
     );
   }
